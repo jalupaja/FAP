5b2eb6e4b5bbff07ffea7eb2d3ea566d850101da
Your Name <[emailprotected]>
2023-05-14 11:02:50 +0200

    Add Numpad to Login Activity

--- app/src/main/java/com/example/fap/Login.kt
+++ app/src/main/java/com/example/fap/Login.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.ImageButton
 import com.example.fap.databinding.ActivityLoginBinding
 import com.google.android.material.textfield.TextInputEditText
@@ -17,6 +18,17 @@ class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var textLogin: TextInputEditText
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var btn4: Button
+    private lateinit var btn5: Button
+    private lateinit var btn6: Button
+    private lateinit var btn7: Button
+    private lateinit var btn8: Button
+    private lateinit var btn9: Button
+    private lateinit var btnBack: ImageButton
+    private lateinit var btn0: Button
     private lateinit var btnLogin: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +39,58 @@ class Login : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // Set up the user interaction to manually show or hide the system UI.
         textLogin = binding.passwordInput
-        btnLogin = binding.btnLogin
+        btnLogin = binding.btnLoginAccept
+        btn1 = binding.btnLogin1
+        btn2 = binding.btnLogin2
+        btn3 = binding.btnLogin3
+        btn4 = binding.btnLogin4
+        btn5 = binding.btnLogin5
+        btn6 = binding.btnLogin6
+        btn7 = binding.btnLogin7
+        btn8 = binding.btnLogin8
+        btn9 = binding.btnLogin9
+        btn0 = binding.btnLogin0
+        btnBack = binding.btnLoginBack
 
-        textLogin.setText("")
+        btn1.setOnClickListener {
+            textLogin.append("1")
+        }
+        btn2.setOnClickListener {
+            textLogin.append("2")
+        }
+        btn3.setOnClickListener {
+            textLogin.append("3")
+        }
+        btn4.setOnClickListener {
+            textLogin.append("4")
+        }
+        btn5.setOnClickListener {
+            textLogin.append("5")
+        }
+        btn6.setOnClickListener {
+            textLogin.append("6")
+        }
+        btn7.setOnClickListener {
+            textLogin.append("7")
+        }
+        btn8.setOnClickListener {
+            textLogin.append("8")
+        }
+        btn9.setOnClickListener {
+            textLogin.append("9")
+        }
+        btn0.setOnClickListener {
+            textLogin.append("0")
+        }
 
-        // TODO textLogin onEnter...
+        btnBack.setOnClickListener {
+            val current = textLogin.text?.toString()
+            if (!current.isNullOrEmpty()) {
+                textLogin.setText(current?.substring(0, current.length - 1))
+            }
+        }
+
         // TODO single line login text
 
         textLogin.setOnEditorActionListener { _, actionId, _ ->
@@ -48,6 +105,9 @@ class Login : AppCompatActivity() {
         btnLogin.setOnClickListener {
             checkPassword()
         }
+
+
+        textLogin.setText("")
     }
 
     private fun checkPassword() {
