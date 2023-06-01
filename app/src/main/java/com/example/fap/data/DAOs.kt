153fdb7df68f137d0bd69ae640e852d41353daec
Jakob Pistohl <j.pistohl@tutanota.com>
2023-06-02 00:52:53 +0200

    DB Change
    - Removed unnecessary ids
    - Added conflict fix for insertCategory

--- app/src/main/java/com/example/fap/data/DAOs.kt
+++ app/src/main/java/com/example/fap/data/DAOs.kt
@@ -3,6 +3,7 @@ package com.example.fap.data
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import java.util.Date
@@ -18,7 +19,7 @@ interface FapDao {
 
     // Wallet
     @Query("SELECT * FROM Wallet WHERE userId = :userId")
-    suspend fun getWallets(userId: Int): List<Wallet>
+    suspend fun getWallets(userId: String): List<Wallet>
 
     @Insert
     suspend fun insertWallet(wallet: Wallet): Long
@@ -36,20 +37,20 @@ interface FapDao {
     @Query("SELECT * FROM Payment WHERE userId = :userId")
     suspend fun getPayments(userId: String): List<Payment>
 
-    @Query("SELECT * FROM Payment WHERE userId = :userId AND walletId = :walletId")
-    suspend fun getPaymentsByWallet(userId: String, walletId: Int): List<Payment>
+    @Query("SELECT * FROM Payment WHERE userId = :userId AND wallet = :wallet")
+    suspend fun getPaymentsByWallet(userId: String, wallet: String): List<Payment>
 
     @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND isPayment = 1")
-    suspend fun getTotalAmountSpent(userId: String): Double
+    suspend fun getTotalAmountSpent(userId: String): Double?
 
-    @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND isPayment = 1 AND walletId = :walletId")
-    suspend fun getTotalAmountSpentByWallet(userId: String, walletId: Int): Double
+    @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND isPayment = 1 AND wallet = :wallet")
+    suspend fun getTotalAmountSpentByWallet(userId: String, wallet: String): Double?
 
-    @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND categoryId = :categoryId AND isPayment = 1")
-    suspend fun getTotalAmountSpentByCategory(userId: String, categoryId: Int): Double
+    @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND category = :category AND isPayment = 1")
+    suspend fun getTotalAmountSpentByCategory(userId: String, category: String): Double?
 
     @Query("SELECT SUM(price) FROM Payment WHERE userId = :userId AND isPayment = 0")
-    suspend fun getTotalIncome(userId: String): Double
+    suspend fun getTotalIncome(userId: String): Double?
 
     @Query("SELECT * FROM payment WHERE userId = :userId AND isPayment = 0 AND date >= :startDate AND date <= :endDate")
     suspend fun getIncomeInRange(userId: String, startDate: Date, endDate: Date): List<Payment>
@@ -57,19 +58,19 @@ interface FapDao {
     @Query("SELECT * FROM payment WHERE userId = :userId AND isPayment = 1 AND date >= :startDate AND date <= :endDate")
     suspend fun getAmountSpentInRange(userId: String, startDate: Date, endDate: Date): List<Payment>
 
-    @Query("SELECT * FROM payment WHERE userId = :userId AND isPayment = 1 AND date >= :startDate AND date <= :endDate AND categoryId = :categoryId")
-    suspend fun getAmountSpentInRangeByCategory(userId: String, startDate: Date, endDate: Date, categoryId: Int): List<Payment>
+    @Query("SELECT * FROM payment WHERE userId = :userId AND isPayment = 1 AND date >= :startDate AND date <= :endDate AND category = :category")
+    suspend fun getAmountSpentInRangeByCategory(userId: String, startDate: Date, endDate: Date, category: String): List<Payment>
 
     // Category
-    @Query("SELECT * FROM Category WHERE userId = :userId")
-    suspend fun getCategories(userId: Int): List<Category>
+    @Query("SELECT * FROM Category")
+    suspend fun getCategories(): List<Category>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertCategory(category: Category): Long
 
     // SavingsGoal
     @Query("SELECT * FROM SavingsGoal WHERE userId = :userId")
-    suspend fun getSavingsGoals(userId: Int): List<SavingsGoal>
+    suspend fun getSavingsGoals(userId: String): List<SavingsGoal>
 
     @Insert
     suspend fun insertSavingsGoal(savingsGoal: SavingsGoal): Long
