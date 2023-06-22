71c5a52b74673e3eeadab4103f22568e1ac510ff
Jakob Pistohl <j.pistohl@tutanota.com>
2023-06-22 17:25:49 +0200

    Add Indicator which Wallet is active

--- app/src/main/java/com/example/fap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fap/ui/home/HomeFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import com.example.fap.R
 import com.example.fap.data.FapDatabase
 import com.example.fap.databinding.FragmentHomeBinding
 import com.example.fap.utils.SharedCurrencyManager
@@ -25,6 +31,10 @@ class HomeFragment : Fragment() {
     // TODO in HomeFragment:
     private var wallets = ArrayList<WalletInfo>()
     private lateinit var walletAdapter: HomeAdapter
+    private lateinit var viewpager: ViewPager2
+    private lateinit var indicatorText: TextView
+    private lateinit var indicatorLeft: ImageView
+    private lateinit var indicatorRight: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,9 +48,27 @@ class HomeFragment : Fragment() {
 
         val view = binding.root
 
-        val viewpager= binding.viewpagerHome
+        viewpager = binding.viewpagerHome
+        indicatorText = binding.indicatorText
+        indicatorLeft = binding.indicatorLeft
+        indicatorRight = binding.indicatorRight
+
         walletAdapter = HomeAdapter(wallets)
         viewpager.adapter = walletAdapter
+
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                updateIndicator(position)
+            }
+        })
+
+        indicatorLeft.setOnClickListener {
+            viewpager.setCurrentItem(viewpager.currentItem - 1, true)
+        }
+
+        indicatorRight.setOnClickListener {
+            viewpager.setCurrentItem(viewpager.currentItem + 1, true)
+        }
 
         return view
     }
@@ -101,4 +129,24 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+    private fun updateIndicator(position: Int) {
+        val amount = wallets.size
+
+        indicatorText.text = wallets[position].walletName
+
+        if (position == 0) {
+            // position 0 always means "All Wallets"
+            indicatorText.text = getString(R.string.all_wallets)
+
+            indicatorLeft.visibility = View.INVISIBLE
+        } else {
+            indicatorLeft.visibility = View.VISIBLE
+        }
+
+        if (position == amount - 1) {
+            indicatorRight.visibility = View.INVISIBLE
+        } else {
+            indicatorRight.visibility = View.VISIBLE
+        }
+    }
 }
